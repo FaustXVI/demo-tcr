f620d6374a553d95ba8543ef0b49f54edf2b17f3
FaustXVI <xavier.detant@gmail.com>
2018-11-23 08:25:43 +0100

    Merge all scripts

--- tcrdd.sh
+++ tcrdd.sh
@@ -1,5 +1,39 @@
 #!/usr/bin/env bash
-source commons.sh
+BRANCH=origin/master
+
+function runTest() {
+    ./gradlew test
+}
+
+function isRed(){
+    [[ ! -z `git diff | grep "^\+.*@Test"` ]]
+}
+
+function isGettingGreen(){
+    [[ ! -z `git diff ${BRANCH} HEAD` ]]
+}
+
+function commit() {
+    git add . && \
+    if isRed; then
+        git commit
+    elif isGettingGreen; then
+        git commit --amend --no-edit
+    else
+        git commit --allow-empty-message -m ""
+    fi
+}
+
+function revert() {
+    git reset --hard
+    git clean -f
+}
+
+function sync() {
+    git rebase ${BRANCH} \
+    && runTest \
+    && git push
+}
 
 if isRed
 then
